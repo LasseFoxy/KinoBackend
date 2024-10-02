01d2cd59461122e677db00dbafb018901f62accc
Meller11 <mael0003@stud.kea.dk>
2024-10-02 15:15:28 +0200

    Ændret i TheaterService

--- src/main/java/com/example/kinobackend/service/TheaterService.java
+++ src/main/java/com/example/kinobackend/service/TheaterService.java
@@ -21,20 +21,7 @@ public class TheaterService {
     }
 
 
-    public Theater updateTheater(int id, Theater theaterDetails) {
-        Optional<Theater> optionalTheater = theaterRepository.findById(id);
 
-        if (optionalTheater.isPresent()) {
-            Theater existingTheater = optionalTheater.get();
-            existingTheater.setName(theaterDetails.getName());
-            existingTheater.setNumber_of_rows(theaterDetails.getNumber_of_rows());
-            existingTheater.setSeats_per_row(theaterDetails.getSeats_per_row());
-
-            return theaterRepository.save(existingTheater);
-        } else {
-            throw new ResourceNotFoundException("Biografsal med id " + id + " ikke fundet.");
-        }
-    }
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public class ResourceNotFoundException extends RuntimeException {
         public ResourceNotFoundException(String message) {
